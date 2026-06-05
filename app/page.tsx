f69bc2d5fbf204c12d28d1a67d1316b45c17ed83
Raphael do Amarante Gomes <raphaag10@gmail.com>
2026-06-05 16:59:02 -0300

    Adição tela de login

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,44 @@
 import Image from "next/image";
 
-export default function Home() {
+
+export default function Login() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+ <div className="flex max-h-screen min-h-screen items-center justify-center bg-gray-100 p-10">
+
+  <div id="login-card" className="flex flex-col md:flex-row md:h-150 w-full max-w-4xl bg-white rounded-2xl shadow-xl overflow-hidden h-auto">
+    
+    <div id="left-side" className="flex flex-col w-full md:w-1/2 p-6 sm:p-10 justify-center">
+      <div id="login-title" className="text-center">
+        <h3 className="text-2xl font-bold mb-4">LOGIN</h3>
+       
+        <form className="flex flex-col justify-center items-center w-full  p-10 space-y-2">
+           <p className="text-gray-600 mb-8">Acesse sua conta</p>
+          <div id="user" className="flex items-center bg-gray-100 rounded-lg px-4 py-3 w-full max-w-sm border border-transparent focus-within:border-blue-500 transition-all ">
+            <span className="text-gray-400 mr-3">👤</span>
+            <input type="text" placeholder="Usuário" className="bg-transparent w-full outline-none text-sm text-gray-700"/>
+          </div>
+          <div id="password" className="flex items-center bg-gray-100 rounded-lg px-4 py-3 w-full max-w-sm border border-transparent focus-within:border-blue-500 transition-all ">
+            <span className="text-gray-400 mr-3">🔒</span>
+            <input type="password" placeholder="Senha" className="bg-transparent w-full outline-none text-sm text-gray-700"/>
+          </div>
+          <button id="login-button" className="w-full max-w-sm mt-6 bg-linear-to-r from-sulfibra-100 to-sulfibra-200 hover:from-sky-600 hover:to-sky-800 text-white font-medium py-3 rounded-xl shadow-md transition-colors text-sm">
+            Entrar
+          </button>
+        </form>
     </div>
-  );
+    </div>
+
+    <div className="hidden md:block md:w-1/2 bg-sulfibra-200">
+      <Image 
+        src="/logo.png"       
+        alt="Logo Sulfibra" 
+        width={300}          
+        height={100}         
+        className="object-contain"
+      />
+    </div>
+
+  </div>
+</div>
+  )
 }
